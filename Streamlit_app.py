5aa5a31e7cf1b59861acc76bbe2f9e3d92d8656b
Adeku5 <114456643+Adeku5@users.noreply.github.com>
2022-09-26 22:06:45 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,3 @@
-import streamlit
+import pandas
 
-streamlit.header  ('Breakfast Menu')
-
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinach & Rocjet Smoothie')
-streamlit.text ('Hard Boiled Free-Range Egg')
-
+my_fruity_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
